375ccb2e507abf8921117caa92a1ec9d44662126
peanut-lover <jihoson94@gmail.com>
2021-07-07 15:12:32 +0900

    Update: add warning massage in login form

--- baemin-register/public/javascripts/login/index.js
+++ baemin-register/public/javascripts/login/index.js
@@ -1,35 +1,54 @@
 const tag = "[LoginPage]";
 
-// TODO: move utils javascript folder.
 const isEmptyText = (text) => {
   return text.length == 0;
 };
 
-const handleLoginInputEvent = (_) => {
+const validateLoginForm = () => {
   const $idInput = document.querySelector("#login-id-input");
   const $pwInput = document.querySelector("#login-pw-input");
-  const $loginButton = document.querySelector("#login-btn");
+  const $idWarningMsg = document.querySelector(
+    ".login-form--id-warning-message"
+  );
+  const $pwWarningMsg = document.querySelector(
+    ".login-form--pw-warning-message"
+  );
 
   if ($idInput && $pwInput) {
     const { value: id } = $idInput;
     const { value: pw } = $pwInput;
 
-    if (!isEmptyText(id) && !isEmptyText(pw)) {
-      console.log(tag, "Activate Login Button.");
-      $loginButton.removeAttribute("disabled");
+    if (isEmptyText(id)) {
+      $idInput.classList.add("error");
+      $idWarningMsg.classList.add("show");
+      return false;
     } else {
-      console.log(tag, "InActivate Login Button.");
-      $loginButton.setAttribute("disabled", true);
+      $idInput.classList.remove("error");
+      $idWarningMsg.classList.remove("show");
     }
+
+    if (isEmptyText(pw)) {
+      $pwInput.classList.add("error");
+      $pwWarningMsg.classList.add("show");
+      return false;
+    } else {
+      $pwInput.classList.remove("error");
+      $idWarningMsg.classList.remove("show");
+    }
+    return true;
+  }
+};
+
+const handleLoginBtnClick = (event) => {
+  if (!validateLoginForm()) {
+    // Prevent submit event;
+    event.preventDefault();
   }
 };
 
 function bindEvents() {
-  const $idInput = document.querySelector("#login-id-input");
-  const $pwInput = document.querySelector("#login-pw-input");
-
-  $idInput.addEventListener("input", handleLoginInputEvent);
-  $pwInput.addEventListener("input", handleLoginInputEvent);
+  const $loginBtn = document.querySelector("#login-btn");
+  $loginBtn.addEventListener("click", handleLoginBtnClick);
 }
 
 function main() {
