84c10ea6dba23d159f5869d4a9a98ba999fd3321
edegiil <electropark917@gmail.com>
2021-07-08 18:16:17 +0900

    Update: 휴대전화 검증 코드 추가

--- baemin-register/routes/auth/registerPhone.js
+++ baemin-register/routes/auth/registerPhone.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { RegisterInfo } from "../../models/register/RegisterInFo.js";
+import checkRegex from '../../public/javascripts/utils/checkRegex.js'
 
 const router = express.Router();
 
@@ -13,18 +14,22 @@ router.get("/", function (req, res) {
     req.session[SESSION_REGISTER_KEY] = null;
   }
 
-  res.render("auth/registerPhone", { title: "Register Phone" });
+  res.render("auth/registerPhone");
 });
 
 router.post("/", function (req, res) {
   const { phone_number } = req.body;
 
-  if (phone_number) {
+  const isValid = checkRegex.phone(phone_number);
+
+  if (isValid) {
     const registerInfo = new RegisterInfo();
     registerInfo.phoneNumber = phone_number;
     req.session[SESSION_REGISTER_KEY] = registerInfo;
 
     res.redirect("/auth/registerDetail");
+  } else {
+    res.render("auth/registerPhone");
   }
 });
 
