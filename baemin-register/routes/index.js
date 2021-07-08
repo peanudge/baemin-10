628b86117c5530cdf2610736a77596875cddf5c2
edegiil <electropark917@gmail.com>
2021-07-08 17:20:53 +0900

    Update: 넘겨주는 파라미터 의미부여

--- baemin-register/routes/index.js
+++ baemin-register/routes/index.js
@@ -4,9 +4,10 @@ var router = express.Router();
 const SESSION_AUTH_KEY = "auth";
 
 router.get("/", function (req, res, next) {
+  const isAuth = req.session.hasOwnProperty(SESSION_AUTH_KEY);
   const nickname = req.session[SESSION_AUTH_KEY]?.nickname;
 
-  res.render("index", { nickname });
+  res.render("index", { isAuth, nickname });
 });
 
 export default router;
